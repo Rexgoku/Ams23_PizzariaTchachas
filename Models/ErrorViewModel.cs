e3ccc0ec3e83f28431bdffa1eafe057e8cd5a9be
José Arthur <josearthurcamilo.30@gmail.com>
2023-10-23 14:23:45 -0300

    InitialProject

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AMS23_caroucel.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ wwwroot/js/quack.js
@@ -0,0 +1,6 @@
+function quaks(q)
+{
+  let quack = new Audio();
+  quack.src = "js/quack_5_sa1UG1N.mp3";
+  quack.play();
+}
